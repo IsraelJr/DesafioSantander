62f6b01a0201128f47afb7768ec8bc72a9408d7f
IsraelJr <israel.santos.junior@everis.com>
2019-06-25 17:12:22 -0300

    Implementados os componentes das Views, restando implementar a Table View

---
+++ DesafioSantander/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesafioSantander
+//
+//  Created by Israel Alves Santos Junior on 25/06/19.
+//  Copyright © 2019 Israel Alves Santos Junior. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
